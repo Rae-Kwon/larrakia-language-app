e92085b6a4638d65f260f09b1068ee73630cda89
Richard Kwon <rkminjae@gmail.com>
2022-02-03 12:40:28 -0500

    change connection to connectionString

--- knexfile.js
+++ knexfile.js
@@ -33,7 +33,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      connection: process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false }
     },
     pool: {
